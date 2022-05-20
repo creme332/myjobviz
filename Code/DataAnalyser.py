8aa2659a33ec78188e6dd7db6a223fa6d19ddb1d
creme332 <65414576+creme332@users.noreply.github.com>
2022-05-20 10:51:31 +0400

    separated Bash/Shell and completed AnalyseDatabase()

--- Code/DataAnalyser.py
+++ Code/DataAnalyser.py
@@ -22,7 +22,7 @@ pd.set_option('display.max_rows', 500)
 # construct a separate csv file for word count
 
 
-def AnalyseLanguages():
+def AnalyseLanguages(destination_filename):
     # =============================================================================
     #     header = "C++,Java,Python,Javascript,\
     #     PHP,HTML,CSS,Node.js,Clojure,C#,Bash/Shell,\
@@ -32,65 +32,117 @@ def AnalyseLanguages():
     language_count = {
         "C++": 0, "Java": 0, "Python": 0, "Javascript": 0, "PHP": 0,
         "HTML": 0, "CSS": 0, "Node.js": 0, "Clojure": 0,
-        "C#": 0, "Bash/Shell": 0, "PowerShell": 0, "Kotlin": 0,
+        "C#": 0, "Bash": 0, "Shell": 0, "PowerShell": 0, "Kotlin": 0,
         "Rust": 0, "Typescript": 0, "SQL": 0, "Ruby": 0, "Dart": 0
     }
+    # Note : SQL is a subtstring of mySQL, NoSQL
 
-    # subtring testing : c++, c#, Node.js, bash/shell
     # languages that contain special characters.
-    special_languages = ["C++", "Node.js", "C#", "Bash/Shell"]
+    special_languages = ["C++", "Node.js", "C#"]
     # These languages are never substrings of other another language.
 
     # token testing : java, python, javascript, php, html, ...
     standard_languages = ["Java", "Python", "Javascript",
-                         "PHP", "HTML", "CSS", "Clojure",
-                         "PowerShell", "Kotlin", "Rust", "Typescript",
-                         "SQL", "Ruby", "Dart"]
+                          "PHP", "HTML", "CSS", "Clojure",
+                          "PowerShell", "Kotlin", "Rust", "Typescript",
+                          "SQL", "Ruby", "Dart", "Bash", "Shell"]
     # Note Java is  a substring of Javascript
     # so substring method cannot be used here.
     # Ruby is a substring of "Ruby on Rails"
-
-
     # languages in language_count, special_languages, standard language must match exactly
-    for row in range(len(jobs_df)):
-        jobs_details = jobs_df.loc[row, "job_details"]
 
-        # search languages using token method
-        words = re.findall(r'\w+', jobs_details)  # uses any symbol as string
-        # delimiters to create list of words from job_details
-        for i in range (0,len(words)):
-            for lang in standard_languages:
-                if lang.lower() == words[i].lower():
-                    if(lang=="Ruby"):
-                        if(i<len(words)-2):
-                            if(words[i+1].lower() != "on" and words[i+2].lower() != "rails"):
-                                language_count[lang] += 1
-                        else :
+    for row in range(len(jobs_df)):  # for each collected job details
+        jobs_details = jobs_df.loc[row, "job_details"].lower()
+
+        # 1st search : search for languages using token method
+        # any symbol is used as string delimiter
+        # list of words in job details
+        words = re.findall(r'\w+', jobs_details)
+
+        for i in range(0, len(words)):  # for each word in job details
+            for lang in standard_languages:  # search for standard languages
+
+                if lang.lower() == words[i]:
+                    if(lang.lower() == "ruby"):  # distinguish between ruby and ruby on rails
+                        if(i > len(words)-3):
+                            # language is definitely ruby
                             language_count[lang] += 1
-                        
-                    else :
-                        language_count[lang] += 1
+                        else:
+                            if(words[i+1].lower() != "on" and words[i+2].lower() != "rails"):
+                                # current word is not part of ruby on rails
+                                language_count[lang] += 1
+                    else:
+                        language_count[lang] += 1  # other standard language
 
         # search special languages using substring
         for lang in special_languages:
-            if lang.lower() in jobs_details.lower():
+            if lang.lower() in jobs_details:
                 language_count[lang] += 1
 
     # save language_count to a csv file
-    lang_filename = "LanguageCountData.csv"
-    tester = pd.DataFrame(language_count.items(), columns=['lang', 'freq'])
-    tester.to_csv(lang_filename, sep='\t', encoding='utf-8-sig', index=False)
+    tester = pd.DataFrame(language_count.items(), columns=[
+                          'Language', 'Frequency'])
+    tester.to_csv(destination_filename, sep='\t',
+                  encoding='utf-8-sig', index=False)
 
-    # display(tester)
+    display(tester)
 
 
-def AnalyseDatabases():
+def AnalyseDatabases(destination_filename):
     databases_count = {
         "MySQL": 0, "PostgreSQL": 0, "SQLite": 0, "MongoDB": 0,
         "Microsoft SQL Server": 0, "Redis": 0, "MariaDB": 0, "Firebase": 0,
         "Elasticsearch": 0, "Oracle": 0, "DynamoDB": 0, "Cassandra": 0,
-        "IBM DB2": 0, "Couchbase": 0
+        "IBM DB2": 0, "Couchbase": 0, "NoSQL": 0
     }  # Will misflag Oracle Cloud as Oracle database
+
+    for row in range(len(jobs_df)):
+        jobs_details = jobs_df.loc[row, "job_details"].lower()  # lower case
+
+        # use substring method for database names with more than 1 word
+        if("microsoft sql server" in jobs_details):
+            databases_count["Microsoft SQL Server"] += 1
+
+        if("ibm db2" in jobs_details):
+            databases_count["IBM DB2"] += 1
+
+        # use token method for single-word databases
+        words = re.findall(r'\w+', jobs_details)
+
+        for db in databases_count:
+            if db.lower() in words:
+                databases_count[db] += 1
+
+    # save databases_count to a csv file
+    lang_filename = destination_filename
+    tester = pd.DataFrame(databases_count.items(), columns=[
+                          'Database', 'Frequency'])
+    tester.to_csv(lang_filename, sep='\t', encoding='utf-8-sig', index=False)
+
+    display(tester)
+
+
+def AnalyseWebFrameworks():
+    web_frameworks = {"Svelte": 0,
+                      "ASP.NET Core": 0,
+                      "FastAPI": 0,
+                      "React.js": 0,
+                      "Vue.js": 0,
+                      "Express": 0,
+                      "Spring": 0,
+                      "Ruby on Rails": 0,
+                      "Angular": 0,
+                      "Django": 0,
+                      "Laravel": 0,
+                      "Flask": 0,
+                      "Gatsby": 0,
+                      "Symfony": 0,
+                      "ASP.NET": 0,
+                      "jQuery": 0,
+                      "Drupal": 0,
+                      "Angular.js": 0
+                      }
+    special_web_frameworks = ["ASP.NET Core", "React.js"]
 
     for row in range(len(jobs_df)):
         jobs_details = jobs_df.loc[row, "job_details"]
@@ -109,6 +161,7 @@ def AnalyseDatabases():
 
 def AnalyseOtherTools():
 
+    # for the following tools/platforms/libraries, substring method only is required
     cloud_platforms = {"AWS": 0,
                        "Google Cloud Platform": 0,
                        "Microsoft Azure": 0,
@@ -164,53 +217,36 @@ def AnalyseOtherTools():
         "Yarn": 0,
         "Unreal Engine": 0,
         "Unity 3D": 0,
+        "GitHub": 0,
     }  # Git is a substring of GitHub
 
     # word frequency of Linux, GitHub
 
     for row in range(len(jobs_df)):
         jobs_details = jobs_df.loc[row, "job_details"]
-        words = re.findall(r'\w+', jobs_details)
+        words = jobs_details.text.lower()
 
         # search cloud platforms
         for cp in cloud_platforms:
-            if cp.lower() in words.lower():
+            if cp.lower() in words:
                 cloud_platforms[cp] += 1
 
         # search web frameworkds
         for wb in web_frameworks:
-            if wb.lower() in words.lower():
+            if wb.lower() in words:
                 web_frameworks[wb] += 1
 
         # search other
         for lb in libraries:
-            if lb.lower() in words.lower():
+            if lb.lower() in words:
                 libraries[lb] += 1
 
         for tool in other_tools:
-            if tool.lower() in words.lower():
+            if tool.lower() in words:  # must distinguish between git and github
                 other_tools[tool] += 1
 
 
-AnalyseDatabases()
-
-
-# =============================================================================
-# To-Do
-# - Search each language/framework on site to see if spelling matches expected spelling
-# - Create a separate file to store frequencies
-# - Alternate spelling of React.js = ReactJS, React
-# - Black background, change colors of bars
-# - add xlabel, ylabel to parameters
-#    https://towardsdatascience.com/donut-plot-with-matplotlib-python-be3451f22704
-#   create donut plot with percentage
-# =============================================================================
-#
-# =============================================================================
-# =============================================================================
-# =============================================================================
-# =============================================================================
-#
-# =============================================================================
-# =============================================================================
-# =============================================================================
+AnalyseLanguages("LanguageCountData.csv")
+# AnalyseDatabases("DatabasesCountData.csv")
+# AnalyseDatabases("DatabasesCountData.csv")
+# AnalyseDatabases("DatabasesCountData.csv")
