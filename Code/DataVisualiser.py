89f0f91aa43ae1badafa203666d1e3065f5244c9
creme332 <65414576+creme332@users.noreply.github.com>
2022-06-10 07:56:52 +0400

    modified hover in map + other  aesthetic changes to barcharts
    changed colors of charts, removed 0 frequency data

--- Code/DataVisualiser.py
+++ Code/DataVisualiser.py
@@ -29,6 +29,10 @@ pd.set_option('display.max_rows', 500)
 def HorizontalLollipopChart(source_filename, destination_filename, title):
     # using data from csv file
     df = pd.read_csv(source_filename, sep='\t')
+
+    # get rid of data with 0 frequency
+    df = df[df['Frequency'] != 0]
+
     column_headings = df.columns
 
     # my_labels = df[column_headings[0]].tolist()
@@ -40,14 +44,14 @@ def HorizontalLollipopChart(source_filename, destination_filename, title):
     # Reorder it based on the values:
     ordered_df = df.sort_values(by=column_headings[1])  # VARIABLE
     my_range = range(1, len(df.index)+1)
-    plt.style.use('ggplot')
+    plt.style.use('fivethirtyeight')
 
     plt.hlines(y=my_range, xmin=0,
-               xmax=ordered_df[column_headings[1]], color='skyblue')
+               xmax=ordered_df[column_headings[1]])  # , color='skyblue'
     plt.plot(ordered_df[column_headings[1]], my_range, "o")
 
     plt.yticks(my_range, ordered_df[column_headings[0]])
-    plt.title(title)
+    plt.title(title, weight='bold')
 
     # plt.savefig(destination_filename, bbox_inches='tight')
     plt.show()
@@ -62,6 +66,9 @@ def HorizontalBarChart(source_filename, destination_filename, my_color, title):
     # =============================================================================
     df = pd.read_csv(source_filename, sep='\t')
     df = df.sort_values(by=['Frequency'])
+
+    # get rid of data with 0 frequency
+    df = df[df['Frequency'] != 0]
 
     column_headings = df.columns
 
@@ -80,7 +87,7 @@ def HorizontalBarChart(source_filename, destination_filename, my_color, title):
     plt.style.use('ggplot')
 
     plt.barh(my_labels, my_data, color=my_color)
-    plt.title(title)
+    plt.title(title, weight='bold')
     # plt.ylabel(column_headings[0])  # VARIABLE
     plt.xlabel(column_headings[1])
     # plt.savefig(destination_filename, bbox_inches='tight')
@@ -126,32 +133,55 @@ def PieChart(source_filename, destination_filename):
     plt.close()
 
 
-def donutChart(source_filename, destination_path):
+def donutChart(source_filename, destination_path, title):
     # https://medium.com/@krishnakummar/donut-chart-with-python-matplotlib-d411033c960b
     df = pd.read_csv(source_filename, sep='\t')
+
+# =============================================================================
+#     # shorten long names for CLoud
+#     df.loc[df.CloudPlatforms == 'Google Cloud Platform',
+#            'CloudPlatforms'] = 'Google Cloud'
+#     print(df)
+# =============================================================================
+    # get rid of data with 0 frequency
+    df = df[df['Frequency'] != 0]
+
     column_headings = df.columns
     my_labels = df[column_headings[0]].tolist()
     my_data = df[column_headings[1]].tolist()
 
-    colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
-    # explode = (1, 0, 0)  # explode a slice if required.
+    # colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
+    colors = ['#ff6384', '#ffce56', '#36a2eb']
+    # explode = (0.1, 0.1, 0.1)  # explode a slice if required.
     # len(explode) = number of rows in df. Then add explode = explode below
 
-    plt.pie(my_data, labels=my_labels, colors=colors,
-            autopct='%1.1f%%', shadow=True)
-    plt.title("Operating systems")
+    plt.pie(my_data, startangle=5, pctdistance=0.75, colors=colors,
+            autopct='%1.0f%%', shadow=True, radius=1.3,
+            # explode=explode,
+            wedgeprops={"edgecolor": "black",
+                        'linewidth': 1,
+                        'antialiased': True}
+            )
+    plt.title(title, weight='bold')
     # draw a circle at the center of pie to make it look like a donut
     centre_circle = plt.Circle(
-        (0, 0), 0.75, color='black', fc='white', linewidth=1.25)
+        (0, 0), 0.65, color='black', fc='white', linewidth=1.0)
     fig = plt.gcf()
     fig.gca().add_artist(centre_circle)
 
     # Set aspect ratio to be equal so that pie is drawn as a circle.
     plt.axis('equal')
+    plt.legend(my_labels, loc="upper right", fontsize=10,
+               bbox_transform=plt.gcf().transFigure)
+    plt.subplots_adjust(left=0.0, bottom=0.1, right=0.95)
+
     plt.show()
 
 
 def CreateMap(source_path, destination_path):
+
+    # PROBLEM WITH LOCATION RIVIEERE DU REMPART - showing SMALL ISLANDS
+    # ADD animations then deploy on github
 
     districts = json.load(open("mauritius-districts-geojson.json", 'r'))
     df = pd.read_csv(source_path, sep='\t')
@@ -159,60 +189,75 @@ def CreateMap(source_path, destination_path):
     # create a log scale to deal with outliers in JobCount
     # get rid of 0s in column (log 0 invalid)
     df["JobCount"].replace(0, 1, inplace=True)
-    df['JobCountScale'] = np.log10(df['JobCount'])
+    df['log(JobCount)'] = np.log10(df['JobCount'])
 
     fig = px.choropleth(df, geojson=districts,
                         featureidkey='properties.name_1',
                         locations='Location',  # column in dataframe which contains districts names
-                        color='JobCountScale',  # data from this column in dataframe is plotted
+                        # data from this column in dataframe is plotted
+                        color='log(JobCount)',
                         color_continuous_scale="turbo",  # turbo or blackbody
-                        range_color=[0, max(df['JobCountScale'])],
-                        labels={"Value": "Count"}
+                        range_color=[0, max(df['log(JobCount)'])],
+                        hover_name='Location',
+                        hover_data={
+                            'JobCount': True,
+                            'Location': False,
+                            'log(JobCount)': False
+                        },
+
+                        # animation_frame="Location",
                         )
     fig.update_geos(fitbounds="locations")
-    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-    fig.write_image(destination_path + ".svg")  # .svg or .pdf
+    fig.update_layout(margin={"r": 300, "t": 50, "l": 300, "b": 50})
+    fig.update_layout(
+        title='May-July 2022 MU IT jobs by District',
+        margin=dict(l=100, r=100, t=100, b=100),
+        font=dict(size=10),
+        hoverlabel=dict(
+            bgcolor="white",
+            font_size=16,
+            font_family="Rockwell"
+        )
+    )
 
-    # fig.show()
+    # customise legend as well #
+    fig.write_html(
+        "choropleth-map-plotly-python.html")
+    # fig.write_image(destination_path + ".pdf")  # .svg or .pdf
+
+    fig.show()
 
 
 def main():
     source_path = 'FilteredData/'  # folder containing filtered data
     destination_path = 'Charts/'  # folder to store charts
 
-    # CreateMap(source_path + "LocationData.csv", destination_path + "JobCountMap")
+    CreateMap(source_path + "LocationData.csv",
+              destination_path + "JobCountMap")
 
-    HorizontalBarChart(source_path + "CloudData.csv",
-                       destination_path + "CloudChart", 'blue', "Cloud platforms")
     HorizontalBarChart(source_path + "DatabaseData.csv",
-                       destination_path + "DatabaseChart", 'green', "Databases")
+                       destination_path + "DatabaseChart", '#5FE916', "Databases")
     HorizontalBarChart(source_path + "LanguageData.csv",
-                       destination_path + "LanguageChart", 'orange', "Programming languages")
+                       destination_path + "LanguageChart", 'orange',
+                       "Programming languages")
     HorizontalBarChart(source_path + "LibrariesData.csv",
-                       destination_path + "LibrariesChart", 'black', "Libraries")
-    HorizontalBarChart(source_path + "OSData.csv",
-                       destination_path + "OSChart", 'red', "Operating systems")
+                       destination_path + "LibrariesChart", '#0FF0A3', "Libraries")
     HorizontalBarChart(source_path + "ToolsData.csv",
-                       destination_path + "ToolsChart", 'purple', "Tools")
+                       destination_path + "ToolsChart", '#a016e9', "Tools")
     HorizontalBarChart(source_path + "WebData.csv",
-                       destination_path + "WebChart", 'skyblue', "Web frameworks")
+                       destination_path + "WebChart", '#F00F5C', "Web frameworks")
 
-    HorizontalLollipopChart(source_path + "WebData.csv",
-                            destination_path + "WebLollipopChart", "Web frameworks")
+    # HorizontalLollipopChart(source_path + "WebData.csv",
+    # destination_path + "WebLollipopChart", "Web frameworks")
 
     # add explanation for percentage in donut chart
-    donutChart(source_path + "OSData.csv", destination_path + "OSChart")
-
-    # HorizontalBarChart(source_path + "", destination_path + "OSData.csv", "")
-
-    # PieChart(source_path + "OSData.csv", destination_path + "")
-    # donutChart(source_path + "WebData.csv", destination_path + "")
-    # PieChart(source_path + "OSData.csv", destination_path + "")
+    donutChart(source_path + "OSData.csv", destination_path +
+               "OSChart", "Operating systems")
+    donutChart(source_path + "CloudData.csv",
+               destination_path + "CloudChart", "Cloud platforms")
 
 
 main()
 source_path = 'FilteredData/'  # folder containing filtered data
 destination_path = 'Charts/'  # folder to store charts
-
-
-# PieChart('FilteredData/OSData.csv', 'Charts/test.pdf')
+# CreateMap(source_path + "LocationData.csv", destination_path + "JobCountMap")
