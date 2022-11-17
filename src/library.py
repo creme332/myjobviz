536c1901ef07711b0190fe9b724a0ffcc4240502
creme332 <65414576+creme332@users.noreply.github.com>
2022-11-17 17:14:32 +0400

    get serviceAccountKey from environment vars

--- src/library.py
+++ src/library.py
@@ -6,8 +6,29 @@ import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
 import pandas as pd
+import os
+from dotenv import load_dotenv, find_dotenv
 
-cred = credentials.Certificate('src/serviceAccount.json')
+
+def getServiceAccountKey():
+    load_dotenv(find_dotenv())
+    variables_keys = {
+        "type": os.getenv("TYPE"),
+        "project_id": os.getenv("PROJECT_ID"),
+        "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+        "private_key": os.getenv("PRIVATE_KEY"),
+        "client_email": os.getenv("CLIENT_EMAIL"),
+        "client_id": os.getenv("CLIENT_ID"),
+        "auth_uri": os.getenv("AUTH_URI"),
+        "token_uri": os.getenv("TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.getenv(
+            "AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL")
+    }
+    return variables_keys
+
+
+cred = credentials.Certificate(getServiceAccountKey())
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 job_collection = db.collection(u'jobs')
@@ -53,3 +74,6 @@ def getAsDataframe():
         df.sort_values('date_posted', ascending=False, inplace=True)
 
     return df
+
+
+print(getAsDataframe().head()['job_title'].values)
