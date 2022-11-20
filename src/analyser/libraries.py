a1ecc7fd1c1cb64867eab915cf6cf182a84646d5
creme332 <65414576+creme332@users.noreply.github.com>
2022-11-20 17:54:31 +0400

    refactor `libraries.py`

--- src/analyser/libraries.py
+++ src/analyser/libraries.py
@@ -1,6 +1,32 @@
 #!venv/bin/python3
 import unittest
 import pandas as pd
+from dictionaryUtils import (toIntegerValues, merge_dicts,
+                             to_true_list, filter_dict)
+
+
+def lib_count(job_details_list) -> dict:
+    count = {".NET Framework": False,
+             "NumPy": False,
+             ".NET Core": False,
+             "Pandas": False,
+             "TensorFlow": False,
+             "React Native": False,
+             "Flutter": False,
+             "Keras": False,
+             "PyTorch": False,
+             "Cordova": False,
+             "Apache Spark": False,
+             "Hadoop": False,
+             "Tableau": False,
+             "Power BI": False,
+             "Power Query": False,
+             }
+    count = toIntegerValues(count)
+    for job_detail in job_details_list:
+        res = toIntegerValues(libraries_check(job_detail))
+        count = merge_dicts(count, res)
+    return count
 
 
 def libraries_check(job_details):
@@ -39,15 +65,14 @@ def libraries_check(job_details):
         if (lang in job_details):
             is_present[key] = True
 
-    # return matches only
-    return [key for key in is_present if is_present[key]]
+    return is_present
 
 
 class TestLibrariesCheck(unittest.TestCase):
 
     def test_long_names(self):
         string = 'Apache Spark'
-        self.assertEqual(libraries_check(string),
+        self.assertEqual(to_true_list(libraries_check(string)),
                          ['Apache Spark'])
 
     def test_all(self):
@@ -60,7 +85,7 @@ class TestLibrariesCheck(unittest.TestCase):
                     'Flutter', 'Keras', 'PyTorch', 'Cordova',
                     'Apache Spark', 'Hadoop', 'Tableau',
                     'Power BI', 'Power Query']
-        self.assertCountEqual(libraries_check(string), expected)
+        self.assertCountEqual(to_true_list(libraries_check(string)), expected)
 
     # @unittest.skip('Reason for skipping')
     def test_real_job_details(self):
@@ -71,10 +96,15 @@ class TestLibrariesCheck(unittest.TestCase):
         df = df[df['job_details'].str.contains(".NET Core")]
         string = df['job_details'].tolist()[0]
         # print(string)
-        self.assertCountEqual(libraries_check(
-            string), ['.NET Core'])
+        self.assertCountEqual(to_true_list(libraries_check(
+            string)), ['.NET Core'])
+
+    def test_count(self):
+        test_list = ['pandas', 'java c#', 'pandas']
+        x = lib_count(test_list)
+        print(filter_dict(x))
+        self.assertEqual(filter_dict(x), {'Pandas': 2})
 
 
 if __name__ == '__main__':
     unittest.main()
-    # web_framework_check('')
