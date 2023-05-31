8b951cf2841bb231ede59f5466f9bbc9137e52a7
creme332 <65414576+creme332@users.noreply.github.com>
2023-05-31 12:19:33 +0400

    rename update_filtered_statistics to update_stats

--- src/anal.py
+++ src/anal.py
@@ -14,41 +14,40 @@ def analyseAndUpdate(my_database, job_details_list,
                      location_list, salary_list):
     # analyse cp
     increment = cp_count(job_details_list)
-    # print(increment)
-    my_database.update_filtered_statistics(
+    my_database.update_stats(
         increment, my_database.cloud_data_ref)
 
     # analyse database
     increment = db_count(job_details_list)
-    my_database.update_filtered_statistics(increment, my_database.db_data_ref)
+    my_database.update_stats(increment, my_database.db_data_ref)
 
     # analyse language
     increment = lang_count(job_details_list)
-    my_database.update_filtered_statistics(increment,
-                                           my_database.lang_data_ref)
+    my_database.update_stats(increment,
+                             my_database.lang_data_ref)
 
     # analyse libraries
     increment = lib_count(job_details_list)
-    my_database.update_filtered_statistics(increment, my_database.lib_data_ref)
+    my_database.update_stats(increment, my_database.lib_data_ref)
 
     # analyse location
     increment = location_count(location_list)
-    my_database.update_filtered_statistics(increment, my_database.loc_data_ref)
+    my_database.update_stats(increment, my_database.loc_data_ref)
 
     # analyse os
     increment = os_count(job_details_list)
-    my_database.update_filtered_statistics(increment, my_database.os_data_ref)
+    my_database.update_stats(increment, my_database.os_data_ref)
 
     # analyse salary
     increment = salary_count(salary_list)
-    my_database.update_filtered_statistics(
+    my_database.update_stats(
         increment, my_database.salary_data_ref)
 
     # analyse tools
     increment = tools_count(job_details_list)
-    my_database.update_filtered_statistics(
+    my_database.update_stats(
         increment, my_database.tools_data_ref)
 
     # analyse web frameworks
     increment = web_count(job_details_list)
-    my_database.update_filtered_statistics(increment, my_database.web_data_ref)
+    my_database.update_stats(increment, my_database.web_data_ref)
