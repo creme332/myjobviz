fdaf909586a397b5b34ea609e136576d407de0f2
creme332 <65414576+creme332@users.noreply.github.com>
2023-06-02 12:23:42 +0400

    reset salaries to initial state

--- backend/src/analyser/salary.py
+++ backend/src/analyser/salary.py
@@ -1,8 +1,8 @@
 def salary_count(salary_list):
-    count = {'Less Than 10,000': 0, '10,000 - 20,000': 0, '21,000 - 30,000': 0,
+    count = {'10,000 - 20,000': 0, '21,000 - 30,000': 0,
              '31,000 - 40,000': 0, '41,000 - 50,000': 0,
              '51,000 - 75,000': 0, '76,000 - 100,000': 0,
-             'More Than 100,000': 0, 'Negotiable': 0, 'Not disclosed': 0
+             'More Than 100,000': 0
              }
     invalid_salaries = ['Not disclosed', 'Negotiable', 'See description']
 
