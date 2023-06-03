a9931a6461f50c723db25d9f7cb0cd75adb9bdb4
creme332 <65414576+creme332@users.noreply.github.com>
2023-06-03 14:32:39 +0400

    use set to prevent duplicates

--- backend/src/utils/constants.py
+++ backend/src/utils/constants.py
@@ -1,124 +1,127 @@
 """
     Module contains constants for list of things to be analysed
 """
-CLOUD_PLATFORMS = ["AWS",
-                   "Google Cloud",
-                   "Azure",
-                   "Heroku",
-                   "DigitalOcean",
-                   "Watson",
-                   "Oracle Cloud Infrastructure"
-                   ]
 
-DATABASES = ["MySQL",
-             "PostgreSQL",
-             "SQLite",
-             "MongoDB",
-             "Microsoft SQL Server",
-             "Redis",
-             "MariaDB",
-             "Firebase",
-             "Elasticsearch",
-             "Oracle",
-             "DynamoDB",
-             "Cassandra",
-             "IBM DB2",
-             "Couchbase",
-             "NoSQL"
-             ]
+# ! Do not use list directly to store values to prevent duplicates
+CLOUD_PLATFORMS = list({"AWS",
+                        "Google Cloud",
+                        "Azure",
+                        "Heroku",
+                        "DigitalOcean",
+                        "Watson",
+                        "Oracle Cloud Infrastructure"
+                        })
 
-LANGUAGES = ["C++",
-             "Java",
-             "Python",
-             "Javascript",
-             "PHP",
-             "HTML",
-             "CSS",
-             "Clojure",
-             "C#",
-             "Bash",
-             "Shell",
-             "PowerShell",
-             "Kotlin",
-             "Rust",
-             "Typescript",
-             "SQL",
-             "Ruby",
-             "Dart"
-             ]
 
-LIBRARIES = [".NET Framework",
-             "NumPy",
-             ".NET Core",
-             "Pandas",
-             "TensorFlow",
-             "React Native",
-             "Flutter",
-             "Keras",
-             "PyTorch",
-             "Cordova",
-             "Apache Spark",
-             "Hadoop",
-             "Tableau",
-             "Power BI",
-             "Power Query"
-             ]
+DATABASES = list({"MySQL",
+                  "PostgreSQL",
+                  "SQLite",
+                  "MongoDB",
+                  "Microsoft SQL Server",
+                  "Redis",
+                  "MariaDB",
+                  "Firebase",
+                  "Elasticsearch",
+                  "Oracle",
+                  "DynamoDB",
+                  "Cassandra",
+                  "IBM DB2",
+                  "Couchbase",
+                  "NoSQL"})
 
-MU_DISTRICTS = ["Black River",
-                "Flacq",
-                "Grand Port",
-                "Moka",
-                "Pamplemousses",
-                "Plaine Wilhems",
-                "Port Louis",
-                "Riviere du Rempart",
-                "Savanne"
-                ]
 
-OPERATING_SYSTEMS = ["Windows",
-                     "Mac",
-                     "Linux"
-                     ]
+LANGUAGES = list({"C++",
+                  "Java",
+                  "Python",
+                  "Javascript",
+                  "PHP",
+                  "HTML",
+                  "CSS",
+                  "Clojure",
+                  "C#",
+                  "Bash",
+                  "Shell",
+                  "PowerShell",
+                  "Kotlin",
+                  "Rust",
+                  "Typescript",
+                  "SQL",
+                  "Ruby",
+                  "Dart"
+                  })
 
-PUBLIC_SALARY_RANGES = ["Less Than 10,000",
-                        "10,000 - 20,000",
-                        "21,000 - 30,000",
-                        "31,000 - 40,000",
-                        "41,000 - 50,000",
-                        "51,000 - 75,000",
-                        "76,000 - 100,000",
-                        "More Than 100,000"
-                        ]
+LIBRARIES = list({".NET Framework",
+                  "NumPy",
+                  ".NET Core",
+                  "Pandas",
+                  "TensorFlow",
+                  "React Native",
+                  "Flutter",
+                  "Keras",
+                  "PyTorch",
+                  "Cordova",
+                  "Apache Spark",
+                  "Hadoop",
+                  "Tableau",
+                  "Power BI",
+                  "Power Query"
+                  })
 
-TOOLS = ["Git",
-         "Terraform",
-         "Kubernetes",
-         "Node.js",
-         "Docker",
-         "Ansible",
-         "Yarn",
-         "Unreal Engine",
-         "Unity 3D",
-         "Github",
-         "Gitlab"
-         ]
+MU_DISTRICTS = list({"Black River",
+                     "Flacq",
+                     "Grand Port",
+                     "Moka",
+                     "Pamplemousses",
+                     "Plaine Wilhems",
+                     "Port Louis",
+                     "Riviere du Rempart",
+                     "Savanne"
+                     })
 
-WEB_FRAMEWORKS = ["Svelte",
-                  "ASP.NET",
-                  "FastAPI",
-                  "React",
-                  "Vue.js",
-                  "Express",
-                  "Spring",
-                  "Ruby on Rails",
-                  "Django",
-                  "Laravel",
-                  "Flask",
-                  "Gatsby",
-                  "Symfony",
-                  "jQuery",
-                  "Drupal",
-                  "Angular.js",
-                  "Angular",
-                  "Next.js"
-                  ]
+OPERATING_SYSTEMS = list({"Windows",
+                          "Mac",
+                          "Linux"
+                          })
+
+PUBLIC_SALARY_RANGES = list({"Less Than 10,000",
+                             "10,000 - 20,000",
+                             "21,000 - 30,000",
+                             "31,000 - 40,000",
+                             "41,000 - 50,000",
+                             "51,000 - 75,000",
+                             "76,000 - 100,000",
+                             "More Than 100,000"
+                             })
+
+TOOLS = list({"Git",
+              "Terraform",
+              "Kubernetes",
+              "Node.js",
+              "Docker",
+              "Ansible",
+              "Yarn",
+              "Unreal Engine",
+              "Unity 3D",
+              "Github",
+              "Gitlab"
+              })
+
+WEB_FRAMEWORKS = list({"Svelte",
+                       "ASP.NET",
+                       "FastAPI",
+                       "React",
+                       "Vue.js",
+                       "Express",
+                       "Spring",
+                       "Ruby on Rails",
+                       "Django",
+                       "Laravel",
+                       "Flask",
+                       "Gatsby",
+                       "Symfony",
+                       "jQuery",
+                       "Drupal",
+                       "Angular.js",
+                       "Angular",
+                       "Next.js"
+                       })
